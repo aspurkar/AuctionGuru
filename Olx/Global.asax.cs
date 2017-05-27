3d250c46a3c07b222be82949ca8d40678330a8c1
Aditya Purkar <aspurkar@gmail.com>
2017-05-26 18:28:36 -0700

    Fianl commit

---
+++ Olx/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using DataLayer;
+using Olx.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace Olx
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public override void Init()
+        {
+            this.AcquireRequestState += MvcApplication_AcquireRequestState;
+        }
+
+        void MvcApplication_AcquireRequestState(object sender, EventArgs e)
+        {
+            var authCookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(authCookie.Value);
+                string email = ticket.Name;
+                var user = new UserReposotiry().GetUserByEmail(email);
+                HttpContext.Current.Session["user"] = user;
+            }
+        }
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
